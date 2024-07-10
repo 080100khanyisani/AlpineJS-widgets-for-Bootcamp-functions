071fa364b08c46322466e06d0312958b81a60c2d
080100khanyisani <khanyisani1010@gmail.com>
2024-07-10 09:30:57 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,128 @@
+function greetApp() {
+    return {
+        name: '',
+        message: '',
+        greet(name) {
+            return "Hello, " + name;
+        },
+        sayHello() {
+            this.message = this.greet(this.name);
+            this.name = ''; // Reset the input field
+        },
+        reset() {
+            this.message = '';
+            this.name = '';
+        }
+    };
+};
+
+function transportFeeApp() {
+    return {
+        shift: '',
+        fee: '',
+        calculateFee() {
+            const lowerCaseShift = this.shift.toLowerCase();
+            if(lowerCaseShift === 'morning') {
+                this.fee = 'R20';
+            } else if(lowerCaseShift === 'afternoon') {
+                this.fee = 'R10';
+            } else if(lowerCaseShift === 'nightshift') {
+                this.fee = 'free';
+            } else {
+                this.fee = 'Invalid shift';
+            }
+        },
+        reset() {
+            this.shift = '';
+            this.fee = '';
+        }
+    };
+};
+
+function airtimeCalculator() {
+    return {
+        usage: '',
+        remainingAmount: '',
+        result: '',
+        calculateAirtime() {
+            const usageArray = this.usage.split(",");
+            const costCall = 1.88;
+            const costBundles = 12;
+            const costSms = 0.75;
+            let costTotal = 0;
+            for (let i = 0; i < usageArray.length; i++) {
+                if (usageArray[i] === "sms") {
+                    costTotal += costSms;
+                } else if (usageArray[i] === "data") {
+                    costTotal += costBundles;
+                } else if (usageArray[i] === "call") {
+                    costTotal += costCall;
+                }
+            }
+            const currentBalance = this.remainingAmount - costTotal;
+            this.result = currentBalance >= 0 ? "R" + currentBalance.toFixed(2) : "R0.00";
+        },
+        reset() {
+            this.usage = '';
+            this.remainingAmount = '';
+            this.result = '';
+        }
+    };
+};
+
+function wordAnalysisApp() {
+    return {
+        sentence: '',
+        results: {
+            longest: '',
+            shortest: '',
+            totalLength: ''
+        },
+        analyzeWords() {
+            this.results.longest = this.longestWord(this.sentence);
+            this.results.shortest = this.shortestWord(this.sentence);
+            this.results.totalLength = this.wordLengths(this.sentence);
+        },
+        reset() {
+            this.sentence = '';
+            this.results.longest = '';
+            this.results.shortest = '';
+            this.results.totalLength = '';
+        },
+        longestWord(sentence) {
+            var words = sentence.split(" ");
+            var longest = "";
+            var longestLength = 0;
+            for (var i = 0; i < words.length; i++) {
+                var cleanWord = words[i].replace(/[^\w\s]/g, "");
+                if (cleanWord.length >= longestLength) {
+                    longest = cleanWord;
+                    longestLength = cleanWord.length;
+                }
+            }
+            return longest;
+        },
+        shortestWord(sentence) {
+            var words = sentence.split(" ");
+            var shortest = "";
+            var shortestLength = Infinity;
+            for (var i = 0; i < words.length; i++) {
+                var cleanWord = words[i].replace(/[^\w\s]/g, "");
+                if (cleanWord.length <= shortestLength && cleanWord.length > 0) {
+                    shortest = cleanWord;
+                    shortestLength = cleanWord.length;
+                }
+            }
+            return shortest;
+        },
+        wordLengths(sentence) {
+            var words = sentence.split(" ");
+            var sumLengths = 0;
+            for (var i = 0; i < words.length; i++) {
+                var cleanWord = words[i].replace(/[^\w\s]/g, "");
+                sumLengths += cleanWord.length;
+            }
+            return sumLengths;
+        }
+    };
+};
